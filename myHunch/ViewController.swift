25bb1be1586534a8f85fceb2596ae04913edc8eb
Arsames Qajar <acqajar@gmail.com>
2016-03-27 02:08:48 -0700

    working math guess, about to add timer

--- myHunch/ViewController.swift
+++ myHunch/ViewController.swift
@@ -9,10 +9,66 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var num1 = Int(arc4random_uniform(6)) + Int(arc4random_uniform(6))
+    var num2 = Int(arc4random_uniform(6)) + Int(arc4random_uniform(6))
+    
+    
+    @IBOutlet var userInput: UITextField!
+    @IBOutlet var postResp: UILabel!
+    @IBOutlet var solveThis: UILabel!
+    
+    
+
+    
+    @IBAction func getResp(sender: AnyObject) {
+        if(userInput == "" && userInput.text == ""){
+            return postResp.text = " NO DONT! "
+        }
+        
+        
+        var total : Int
+
+    
+        
+        total = num1 * num2
+
+        print(userInput)
+        
+        
+        
+        if(userInput != "" && userInput.text != ""){
+            if(Int(userInput.text!)! == total) {
+                postResp.text = "You Rock"
+                num1 = Int(arc4random_uniform(6)) + Int(arc4random_uniform(6))
+                num2 = Int(arc4random_uniform(6)) + Int(arc4random_uniform(6))
+                total = num1 * num2
+                print(total)
+                solveThis.text = "\(num1) * \(num2)"
+                return
+            } else {
+                postResp.text = "Dumbassss"
+                return
+            }
+            
+        }
+        else {
+            postResp.text = "Blank :("
+            print("blank")
+            return
+        }
+        
+        
+        
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+         num1 = Int(arc4random_uniform(6)) + Int(arc4random_uniform(6))
+         num2 = Int(arc4random_uniform(6)) + Int(arc4random_uniform(6))
+        
+        
+        solveThis.text = "\(num1) * \(num2)"
     }
 
     override func didReceiveMemoryWarning() {
@@ -22,4 +78,25 @@ class ViewController: UIViewController {
 
 
 }
+
+
+//        postResp.text = " "
+//        userInput.text = " "
+//        solveThis.text = " "
+
+//        let num1 = Int(arc4random_uniform(6)) + Int(arc4random_uniform(6))
+//        let num2 = Int(arc4random_uniform(6)) + Int(arc4random_uniform(6))
+//
+//
+//
+//
+//
+//        let total = num1 * num2
+//        print(total)
+//        solveThis.text = "\(num1) * \(num2)"
+
+
+
+//        print(Int(userInput.text!)!)
+
 
